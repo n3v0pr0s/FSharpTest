4873ed920b7fdbeffa4a0763e274385fc5a48ece
n3v0pr0s <n3v0pr0s@gmail.com>
2021-02-10 00:53:43 +0500

    init

---
+++ ConsoleFSharpApp/Program.fs
@@ -0,0 +1,26 @@
+﻿open System
+
+let isEven x = 
+    x % 2 = 0
+     
+let getEvenNumbers arr =    
+    Seq.where(fun x -> isEven x) arr        
+    
+
+[<EntryPoint>]
+let main argv =  
+    printf "Введите начало диапазона: "
+    let startInt = Console.ReadLine() |> int  
+
+    printf "Введите конец диапазона: "
+    let endInt = Console.ReadLine() |> int    
+
+    let evenSeq = getEvenNumbers [startInt..endInt] 
+
+    for i in evenSeq do
+        printfn "%i" i
+    
+    printfn "\r\nГотово"
+    Console.ReadKey()
+    0
+
